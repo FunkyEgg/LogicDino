2b0aeff9981b52e134e5e6974afa22819ad0998c
Matthew Harris <matt.harris2908@gmail.com>
2022-05-30 18:34:57 +1000

    first ver

--- .github/README.md
+++ .github/README.md
@@ -4,7 +4,7 @@ A logic libary for deno
 ## Example
 
 ```ts
-import { and, or, bit } from '../src/mod.ts';
+import { and, or, bit } from 'https://deno.land/x/logicdino@0.1.0/mod.ts';
 
 function xor(in1: bit, in2: bit): bit {
     if (or(in1, in2) === 1 && and(in1, in2) === 0) {
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,4 +1,4 @@
-import { and, not, or, bit, bits, hexbit, hexbits, parseBit } from '../src/mod.ts';
+import { and, not, or, bit, bits, hexbit, hexbits, parseBit } from 'https://deno.land/x/logicdino@0.1.0/mod.ts';
 
 function xor(in1: bit, in2: bit): bit {
     if (or(in1, in2) === 1 && and(in1, in2) === 0) {
